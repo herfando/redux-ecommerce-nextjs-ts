dac93f63906d0984b95cba45a82d7d383f5c152b
Herfando <herfando_91@yahoo.com>
2025-10-26 05:53:49 +0700

    create navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+
+export default function Navbar() {
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  const user = useSelector((state: RootState) => state.user);
+
+  return (
+    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <Link href="/" className="font-bold text-xl">E-Shop</Link>
+      <div className="flex items-center gap-4">
+        {user.loggedIn ? (
+          <span>Hi, {user.currentUser?.username}</span>
+        ) : (
+          <Link href="/login">Login</Link>
+        )}
+        <Link href="/cart">Cart ({cartItems.length})</Link>
+      </div>
+    </nav>
+  );
+}
